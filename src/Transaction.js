588893b621e616204f4b486655f836497b80749c
Alberto Siless <asiless@gmail.com>
2023-03-01 13:55:01 -0300

    transaction receipt

--- src/Transaction.js
+++ src/Transaction.js
@@ -6,33 +6,43 @@ import { useState } from 'react';
 export const Transaction = ( props ) => {
 
     const [t, setTransaction] = useState(props.transaction);
+    const [r, setReceipt] = useState({});
     const [visible, setVisible] = useState(false);
 
     const handleClick = function() {
+
+        if (Object.keys(r).length === 0) {
+            getReceipt(t.hash)
+            .then(r => {
+              setReceipt(r);
+            });  
+        }
+
         setVisible(!visible);
     }  
     
-    const retieveReceipt = function() {
-        getReceipt(t.hash)
-          .then(r => {
-                t.receipt = r;
-                setTransaction(t);
-                setVisible(visible);
-          });
-      }
-
     const Details = () => (
-        <div className="entries">
-        {
-            Object.keys(t).filter( k => t[k] && k != 'wait').map((key, i) => (
-            <p key={i}>
-                <span className='key'>{key}:</span>
-                <span className='value'>{JSON.stringify(t[key], null, 4)}</span>
-            </p>
-            ))
-        }
-        {!t.receipt?(<a className='value' href='#' onClick={retieveReceipt} id={t.hash}>see receipt</a>):null}
-            
+        <div>
+            <div className="entries">
+            {
+                Object.keys(t).filter( k => t[k] && k != 'wait').map((key, i) => (
+                <p key={i}>
+                    <span className='key'>{key}:</span>
+                    <span className='value'>{JSON.stringify(t[key], null, 4)}</span>
+                </p>
+                ))
+            }
+            </div>
+            <div className='receipt'>
+            {
+                Object.keys(r).map((key, i) => (
+                    <p key={i}>
+                        <span className='key'>{key}:</span>
+                        <span className='value'>{JSON.stringify(r[key], null, 4)}</span>
+                    </p>
+                ))    
+            }            
+            </div>
         </div>
     );
 
